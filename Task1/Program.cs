eaee725b6b3771300717cd3e5035adad290b7b88
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-17 17:11:52 +0800

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write(" Введите пятизначное число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+
+if (stringNumber[0] == stringNumber[4] && stringNumber[1] == stringNumber[3])
+{
+    Console.WriteLine(" Палиндром ");
+}
+else
+{
+    Console.WriteLine(" Не палиндром ");
+}
