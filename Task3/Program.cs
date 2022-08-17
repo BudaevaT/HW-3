4c878b354d798c5592427ae9cc7c2615d453757d
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-18 00:05:25 +0800

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число N");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+            Console.WriteLine(Math.Pow(i, 3));
+    }
+}
